04f1337906c2c55bb6999b71a4f7eb3667a32dd1
CHOULOKY <dlsgml5131@gmail.com>
2024-10-27 20:17:07 +0900

    feat: Setting for Learning left course
    24.10.27
    좌회전 코스 학습을 위한 환경 구축 완료

--- Assets/Scripts/AnimalAgent.cs
+++ Assets/Scripts/AnimalAgent.cs
@@ -5,30 +5,50 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using System.Net;
+using UnityEngine.UIElements;
 
 public class AnimalAgent : Agent
 {
     private Rigidbody rigid;
 
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
     public float moveSpeed;
     public float turnSpeed;
+
+    private int nextCheckpoint = 0;
+    private float directionDot;
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
+
+        startPosition = transform.position;
+        startRotation = transform.rotation;
     }
 
     public override void OnEpisodeBegin()
     {
-        rigid.position = Vector3.zero;
-        rigid.rotation = Quaternion.identity;
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+
+        int randomPosition = UnityEngine.Random.Range(0, 4);
+        rigid.position = new Vector3(startPosition.x, startPosition.y, -4 + randomPosition * 2.3f);
+        // rigid.position = startPosition;
+        rigid.rotation = startRotation;
+
+        nextCheckpoint = 0;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(rigid.velocity);
         sensor.AddObservation(transform.position);
         sensor.AddObservation(transform.rotation);
+
+        Vector3 checkpointForward = GameManager.Instance.checkpoints.checkpoints[nextCheckpoint].transform.forward;
+        directionDot = Vector3.Dot(checkpointForward, transform.forward);
+        sensor.AddObservation(directionDot);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
@@ -36,15 +56,19 @@ public class AnimalAgent : Agent
         float moveZ = actions.DiscreteActions[0]; // 0: 정지, 1: 앞, 2: 뒤
         float moveX = actions.DiscreteActions[1]; // 0: 정지, 1: 오른, 2: 왼
 
-        Vector3 newPosition = Vector3.MoveTowards(rigid.position, transform.forward * (moveZ == 2 ? -1 : moveZ),
-            moveSpeed * Time.deltaTime);
+        Vector3 newPosition = Vector3.MoveTowards(rigid.position,
+            rigid.position + transform.forward * (moveZ == 2 ? -1 : moveZ), moveSpeed * Time.fixedDeltaTime);
         rigid.MovePosition(newPosition);
 
         float turnY = (moveX == 2 ? -1 : moveX) * turnSpeed * Time.fixedDeltaTime;
         Quaternion turnRotation = Quaternion.Euler(0f, turnY, 0f);
         rigid.MoveRotation(rigid.rotation * turnRotation);
 
-        AddReward(-1f / MaxStep); // 빠르게 목표에 다다를 수 있도록 부정적인 보상을 지속적으로
+        if (MaxStep != 0) AddReward(-1f / MaxStep); // 빠르게 목표에 다다를 수 있도록 부정적인 보상을 지속적으로
+        AddReward(directionDot * 0.1f); // 다음 체크포인트의 방향과 일치할수록 보상 증가
+
+        // 현재 누적 보상 출력
+        // Debug.Log($"Current Reward: {GetCumulativeReward()}");
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -62,11 +86,18 @@ public class AnimalAgent : Agent
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Checkpoint")) {
+        if (collision.gameObject.CompareTag("Checkpoint") &&
+            nextCheckpoint == GameManager.Instance.checkpoints.checkpoints.IndexOf(collision.gameObject)) {
             AddReward(1f);
+            if (nextCheckpoint >= GameManager.Instance.checkpoints.checkpoints.IndexOf(collision.gameObject)) {
+                // nextCheckpoint = -1;
+                EndEpisode();
+            }
+            nextCheckpoint++;
         }
         else if (collision.gameObject.CompareTag("Wall")) {
             AddReward(-0.5f);
+            EndEpisode();
         }
     }
 
@@ -76,4 +107,21 @@ public class AnimalAgent : Agent
             AddReward(-0.1f);
         }
     }
+
+    /*
+    private void FixedUpdate()
+    {
+        // test code
+        float moveZ = Input.GetAxisRaw("Vertical");
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        Vector3 newPosition = Vector3.MoveTowards(rigid.position,
+            rigid.position + transform.forward * (moveZ == 2 ? -1 : moveZ), moveSpeed * Time.fixedDeltaTime);
+        rigid.MovePosition(newPosition);
+
+        float turnY = moveX * turnSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turnY, 0f);
+        rigid.MoveRotation(rigid.rotation * turnRotation);
+    }
+    */
 }
